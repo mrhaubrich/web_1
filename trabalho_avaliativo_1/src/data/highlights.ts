05956a21d062d54178e0ea4a9db6919b624a86af
Marco Haubrich <marhaubrich@ucs.br>
2024-08-26 16:29:23 -0300

    chore: Update Astro configuration and add new components

---
+++ trabalho_avaliativo_1/src/data/highlights.ts
@@ -0,0 +1,36 @@
+export const highlights: HighlightModel[] = [
+  {
+    image:
+      "https://www.gamevicio.com/static/imagens_up/big/113/foi-revelado-o-verdadeiro-motivo-de-black-myth-wukong-ter-sido-adiado-no-xbox-series-112773.webp",
+    alt: "Playstation 5 Pro",
+    title:
+      "Foi revelado o verdadeiro motivo de Black Myth: Wukong ter sido adiado no Xbox Series",
+    description:
+      "O jogo já teria sido rejeitado duas vezes nos testes de detecção de erros do Xbox.",
+    href: "/news/black-myth-wukong",
+    tag: "Games",
+  },
+  {
+    image:
+      "https://www.gamevicio.com/static/imagens_up/big/107/por-ate-r-649-99-preco-de-star-wars-outlaws-nao-foi-muito-bem-recebido-pelos-jogadores-106179.jpg",
+    alt: "Playstation 5 Pro",
+    title: "Confira as notas de Star Wars Outlaws",
+    description: "O jogo está programado para ser lançado ainda esta semana.",
+    href: "/news/black-myth-wukong",
+    tag: "Games",
+  },
+];
+
+export const carouselHighlights: HighlightModel[] = [
+  {
+    image:
+      "https://www.gamevicio.com/static/imagens_up/big/106/amd-sugere-que-o-playstation-5-pro-tera-ai-upscaling-105204.jpg",
+    alt: "Playstation 5 Pro",
+    title:
+      "PS5 Pro teria sido mencionado por diversos desenvolvedores durante a Gamescom 2024",
+    description:
+      "Um suposto desenvolvedor adiou o seu jogo por causa do PlayStation 5 Pro.",
+    href: "/news/playstation-5-pro",
+    tag: "Games",
+  },
+];
--- trabalho_avaliativo_1/src/env.d.ts
+++ trabalho_avaliativo_1/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+type HighlightModel = {
+  href: string;
+  title: string;
+  description: string;
+  image: string;
+  alt: string;
+  tag: string;
+};
