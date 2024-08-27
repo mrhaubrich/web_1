517bd24f9569a72b0cfd4cf0ff92cb9186675383
Marco Aurélio da Rosa Haubrich <marcolino.haubrich@gmail.com>
2024-08-26 22:10:07 -0300

    refactor: Add News component to display news articles on the home page

---
+++ trabalho_avaliativo_1/src/data/news.ts
@@ -0,0 +1,41 @@
+import { coca } from "./authors";
+
+export const news: NewsModel[] = [
+  {
+    title:
+      "Indiana Jones ganha trailer no canal oficial do PlayStation e página na PSN",
+    description:
+      "Você pode adicionar Indiana Jones e o Grande Círculo à sua lista de desejos.",
+    alt: "Indiana Jones ganha trailer no canal oficial do PlayStation e página na PSN",
+    author: coca,
+    date: "2024-08-26 18:07",
+    image:
+      "https://www.gamevicio.com/static/imagens_up/big/113/indiana-jones-ganha-trailer-no-canal-oficial-do-playstation-e-pagina-na-psn-112825.webp",
+    slug: "indiana-jones-ganha-trailer-no-canal-oficial-do-playstation-e-pagina-na-psn",
+    type: "#games",
+  },
+  {
+    title:
+      "Estúdio que ajudou a Rare a portar Sea of Thieves para o PS5 também está auxiliando no desenvolvimento de Fable",
+    description: "Third Kind Games está trabalhando com a Playground Games.",
+    alt: "Estúdio que ajudou a Rare a portar Sea of Thieves para o PS5 também está auxiliando no desenvolvimento de Fable",
+    author: coca,
+    date: "2024-08-26 17:07",
+    image:
+      "https://www.gamevicio.com/static/imagens_up/big/113/estudio-que-ajudou-a-rare-a-portar-sea-of-thieves-para-o-ps5-tambem-esta-auxiliando-no-desenvolvimento-de-fable-112821.webp",
+    slug: "estudio-que-ajudou-a-rare-a-portar-sea-of-thieves-para-o-ps5-tambem-esta-auxiliando-no-desenvolvimento-de-fable",
+    type: "#games",
+  },
+  {
+    title: "Shadow of the Road é revelado, novo CRPG steampunk no Japão Feudal",
+    description:
+      "Conheça o novo jogo publicado pela Owlcat Games, estúdio de Pathfinder.",
+    alt: "Shadow of the Road é revelado, novo CRPG steampunk no Japão Feudal",
+    author: coca,
+    date: "2024-08-26 16:09",
+    image:
+      "https://www.gamevicio.com/static/imagens_up/big/113/shadow-of-the-road-e-revelado-novo-crpg-steampunk-no-japao-feudal-112815.webp",
+    slug: "shadow-of-the-road-e-revelado-novo-crpg-steampunk-no-japao-feudal",
+    type: "#games",
+  },
+];
