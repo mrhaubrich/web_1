a7e2d98e80c5651f486f53472579bab4fd74178a
Marco Haubrich <marhaubrich@ucs.br>
2024-08-29 12:27:17 -0300

    refactor: Update NewsHighlight component to use ZoomOnHoverImage instead of ZoomOnHoverImageLink

--- trabalho_avaliativo_1/src/data/highlights.ts
+++ trabalho_avaliativo_1/src/data/highlights.ts
@@ -66,7 +66,64 @@ export const highlights: NewsDetail[] = [
     date: "2024-08-26 09:36",
     cover:
       "https://www.gamevicio.com/static/imagens_up/big/107/por-ate-r-649-99-preco-de-star-wars-outlaws-nao-foi-muito-bem-recebido-pelos-jogadores-106179.jpg",
-    elements: [],
+    elements: [
+      {
+        data: "O jogo Star Wars Outlaws está programado para ser lançado ainda esta semana e como padrão da indústria, o título de mundo aberto da Massive Entertainment foi avaliado pela mídia especializada.",
+        type: "paragraph",
+      },
+      {
+        data: "Atualmente Star Wars Outlaws está disponível no Metacritic com 78 de Metascore (versão PS5) baseado em 25 análises positivas, 7 neutras e nenhuma negativa. No OpenCritic é semelhante - 77 de Top Critic Average e 81% Critics Recommend.",
+        type: "paragraph",
+      },
+      {
+        data: "Além da versão PS5, a versão PC de Star Wars Outlaws também foi avaliada e está com 74 de Metascore, enquanto a versão do Xbox Series está com 77 de Metascore.",
+        type: "paragraph",
+      },
+      {
+        data: '<iframe class="w-full h-auto aspect-video" src="https://www.youtube.com/embed/HlfuN4yj3yg" title="Star Wars Outlaws: Official Launch Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        type: "embed",
+      },
+      {
+        data: '<h3 class="text-lg font-bold">Veja as notas:</h3>',
+        type: "paragraph",
+      },
+      {
+        data: `<ol class="list-disc list-inside leading-9"><li>95 - Gamers Heroes</li><li>90 - Hey Poor Player</li><li>90 - PSX Brasil</li><li>90 - GameSpew</li><li>88 - Voxel</li><li>85 - Carole Quintaine</li><li>85 - CGMagazine</li><li>85 - COGconnected</li><li>85 - GamePro Germany</li><li>85 - GamingTrend</li><li>85 - Vandal</li><li>83 - Player 2</li><li>82 - TierraGamer</li><li>80 - Checkpoint Gaming</li><li>80 - Dexerto</li><li>80 - GameGrin</li><li>80 - Noisy Pixel</li><li>80 - JeuxActu</li><li>80 - Twinfinite</li><li>80 - Screen Rant</li><li>80 - PlayStation Universe</li><li>80 - Everyeye.it</li><li>80 - Gamer.nl</li><li>75 - IGN Benelux</li><li>75 - CD-Action</li><li>70 - Gamer.no</li><li>70 - Malditos Nerds</li><li>70 - IGN</li><li>70 - TheGamer</li><li>60 - GGRecon</li><li>60 - Press Start Australia</li><li>60 - VGC</li><li>50 - Hardcore Gamer</li></ol>`,
+        type: "paragraph",
+      },
+      {
+        data: '<b>Gamers Heroes:</b> "A Massive Entertainment criou ambientes deslumbrantes em toda a galáxia, entregou um sistema de combate acelerado fiel ao estilo de mercenário e criou uma história que rivaliza com os filmes modernos. Star Wars Outlaws é, simplesmente, o jogo de Star Wars de uma geração."',
+        type: "quote",
+      },
+      {
+        data: '<b>Hey Poor Player:</b> "Star Wars Outlaws pode não ser o jogo mais original, mas praticamente tudo nele é bem-executado e um prazer de experimentar. Com uma história excelente, personagens interessantes, ótimas atuações de voz e uma quantidade aparentemente interminável de missões, este é o meu jogo de Star Wars favorito em anos. Os fãs encontrarão muitos motivos para se perder novamente nessa galáxia tão, tão distante."',
+        type: "quote",
+      },
+      {
+        data: '<b>GameSpew:</b> "Star Wars Outlaws captura perfeitamente a essência da amada franquia, fazendo você acreditar que realmente entrou em seu mundo. Os locais são densamente repletos de coisas para fazer, e seja você se esgueirando ou se envolvendo em um tiroteio total, é sempre empolgante. Adicione o sistema de Sindicato, que significa que suas ações têm consequências, e você terá um dos melhores jogos de Star Wars até agora."',
+        type: "quote",
+      },
+      {
+        data: '<b>Dexerto:</b> "Enquanto Star Wars: Outlaws brilha graças ao seu espetacular design de mundo aberto, permitindo que você explore vários planetas gigantescos com uma imensa atenção aos detalhes, uma narrativa segura e bastante convencional diminui um pouco o impacto do pacote, impedindo que seja considerado um clássico absoluto."',
+        type: "quote",
+      },
+      {
+        data: '<b>Gamer.no:</b> "Jogar como um mercenário equipado apenas com astúcia e uma pistola laser é divertido e oferece uma galeria de papéis mais encantadora do que o que costumamos encontrar entre os Cavaleiros Jedi um pouco mais pomposos. No entanto, este não é um jogo que reinventa a roda. Ele se baseia em muitas fórmulas conhecidas e parece se inspirar bastante nos jogos Uncharted na maneira como é jogado."',
+        type: "quote",
+      },
+      {
+        data: '<b>IGN:</b> "Star Wars Outlaws é uma divertida aventura intergaláctica de assalto com uma excelente exploração, mas é prejudicado por uma furtividade simples, combate repetitivo e alguns bugs demais no lançamento."',
+        type: "quote",
+      },
+      {
+        data: '<b>Hardcore Gamer:</b> "A ideia de um jogo de Star Wars focado inteiramente no mundo dos mercenários nas partes mais sombrias do universo é ótima, mas merecia um tipo diferente de jogo do que Star Wars Outlaws. Em vez de ser adequado para algo mais como Shadows of Doubt, Disco Elysium ou até mesmo L.A. Noire, um jogo de ação linear e direto seria melhor. Em vez disso, recebemos um monstro de mundo aberto entediante com uma jogabilidade sem inspiração, muitas características inúteis e um fluxo constante de conteúdo secundário desinteressante. Kay e sua equipe merecem algo muito melhor, assim como os jogadores. Guarde seus créditos e espere por algo mais, como um jogo solo de Sabacc em Kessel."',
+        type: "quote",
+      },
+      {
+        data: "Star Wars Outlaws será lançado em 30 de agosto para PC, PlayStation 5 e Xbox Series S|X.",
+        type: "paragraph",
+      },
+    ],
   },
 ];
 
